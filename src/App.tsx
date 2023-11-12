48f7743faca0cc6b9884495239cdb360c012489b
ikolodin <il.kolodin2015@yandex.ru>
2023-11-12 22:43:23 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { useAppSelector } from './hooks/redux'
+
+function App() {
+  const {increment} = userSlice.actions
+  const dispatch = 
+  return (
+    <div className="App">
+    Hello!!!
+    </div>
+  );
+}
+
+export default App;
